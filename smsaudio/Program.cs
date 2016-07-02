71cc3608b738429ef6eacfc90d4a4f492a272bf4
David Parrott <david.parrott@gmail.com>
2016-07-02 10:03:45 +0100

    Add first functional implementation of the SN76489

--- smsaudio/Program.cs
+++ smsaudio/Program.cs
@@ -1,15 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using static System.Console;
 
 namespace smsaudio
 {
     class Program
     {
+        const int ClockFrequencyNTSC = 3579545; // Hz
+        const int ClockFrequencyPAL = 3546893; // Hz
+
         static void Main(string[] args)
         {
+            using (BinaryWriter writer = new BinaryWriter(File.Open("output.raw", FileMode.Create)))
+            {
+                SN76489 psg = new SN76489(writer);
+
+                //SetTone(psg, 0, 440);
+                //SetTone(psg, 1, 329.6);
+                //SetTone(psg, 2, 392);
+
+                //byte volume = 0;
+
+                //SetVolume(psg, 0, volume);
+                //SetVolume(psg, 1, volume);
+                //SetVolume(psg, 2, volume);
+
+                //SetTone(psg, 2, 10000);
+                //SetVolume(psg, 2, 0);
+
+                psg.WriteData(0xE4);
+                SetVolume(psg, 3, 0);
+
+                // generate 5 seconds worth of audio
+                for (int i = 0; i < 5; i++)
+                {
+                    psg.Update(ClockFrequencyNTSC);
+                }
+            }
+        }
+
+        static int GetPeriodForFrequency(int clockFrequency, double frequency)
+        {
+            // f = clock / (32 x reg)
+            // reg = (clock / f) / 32
+            return (int)(clockFrequency / frequency) / 32;
+        }
+
+        static void SetTone(SN76489 psg, int channel, double frequency)
+        {
+            // get required period for the frequency
+            int period = GetPeriodForFrequency(ClockFrequencyNTSC, frequency);
+
+            // split into lower 4 and upper 6 bits
+            int loData = period & 0x0F;
+            int hiData = (period >> 4) & 0x3F;
+
+            // generate data bytes for psg
+            byte latch = (byte)(0x80 | (channel * 2) << 4 | loData);
+            byte data = (byte)hiData;
+
+            psg.WriteData(latch);
+            psg.WriteData(data);
+        }
+
+        static void SetVolume(SN76489 psg, int channel, byte volume)
+        {
+            byte latch = (byte)(0x80 | ((channel * 2 + 1) << 4) | (volume & 0x0F));
+
+            psg.WriteData(latch);
         }
     }
 }
---
+++ smsaudio/SN76489.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.IO;
+
+namespace smsaudio
+{
+    class SN76489
+    {
+        const int ChannelCount = 4;
+
+        int _clock = 0;
+        double[] _volumeTable;
+
+        ushort[] _registers;
+        int _latchedRegister;
+        bool _toneRegisterLatched;
+
+        int[] _channelCounter;
+        int[] _channelOutput;
+
+        ushort _lsfr = 0x8000;
+
+        BinaryWriter _outputStream;
+
+        public SN76489(BinaryWriter outputStream)
+        {
+            _outputStream = outputStream;
+
+            _registers = new ushort[8];
+            _channelCounter = new int[ChannelCount];
+            _channelOutput = new int[ChannelCount];
+
+            // initialise registers
+            for (int i = 0; i < ChannelCount; i++)
+            {
+                _registers[i * 2] = 0;      // tone/noise
+                _registers[i * 2 + 1] = 0x0F; // volume
+
+                _channelOutput[i] = -1;  // start output high
+            }
+
+            _latchedRegister = 0;
+            _toneRegisterLatched = true;
+
+            _volumeTable = ComputeVolumeLookup();
+        }
+
+        private static double[] ComputeVolumeLookup()
+        {
+            int maxVolume = 8000;
+            double attenuation = Math.Pow(10, -0.1); // 2dB attenuation
+
+            double[] volumeTable = new double[16];
+
+            for (int i = 0; i < volumeTable.Length; i++)
+            {
+                // attenuate 2dB for each entry in table
+                volumeTable[i] = maxVolume * Math.Pow(attenuation, i);
+            }
+
+            // 0x0F is silence
+            volumeTable[15] = 0;
+
+            return volumeTable;
+        }
+
+        public void WriteData(byte data)
+        {
+            // if bit 7 is set it's a latch/data byte
+            if ((data & 0x80) == 0x80)
+            {
+                // update the latched register from the data byte
+                //   %1cctdddd
+                //     |||````--Data
+                //     ||`------Type
+                //     ``-------Channel
+
+                _latchedRegister = (data >> 4) & 0x07;
+                _toneRegisterLatched = (data & 0x10) == 0x00;
+
+                // set the low 4 bits of the latched register
+                _registers[_latchedRegister] = (ushort)((_registers[_latchedRegister] & ~0x0F) | (data & 0x0F));
+
+            }
+            // otherwise it's a data byte
+            else
+            {
+                // if a tone register is latched the lower 6 bits are placed
+                // into the high 6 bits of the register
+                if (_toneRegisterLatched)
+                {
+                    _registers[_latchedRegister] = (ushort)((_registers[_latchedRegister] & 0x0F) | ((data & 0x3F) << 4));
+                }
+                // for a volume register set the low 4 bits of the latched register
+                else
+                {
+                    _registers[_latchedRegister] = (ushort)((_registers[_latchedRegister] & ~0x0F) | (data & 0x0F));
+                }
+            }
+        }
+
+        public void Update(uint cycleCount)
+        {
+            _clock -= (int)cycleCount;
+
+            while (_clock <= 0)
+            {
+                // update tone channels
+                for (int i = 0; i < 3; i++)
+                {
+                    // if the tone register period is 0 then skip
+                    if (_registers[i * 2] == 0)
+                        continue;
+
+                    // update channel counter, if zero flip output and reload from tone register
+                    if (--_channelCounter[i] <= 0)
+                    {
+                        _channelCounter[i] += _registers[i * 2];
+                        _channelOutput[i] *= -1;
+                    }
+                }
+
+                // update noise channel
+                if (--_channelCounter[3] <= 0)
+                {
+                    // reset counter
+                    switch (_registers[6] & 0x03)
+                    {
+                        case 0: _channelCounter[3] += 0x20; break;
+                        case 1: _channelCounter[3] += 0x40; break;
+                        case 2: _channelCounter[3] += 0x80; break;
+                        case 3: _channelCounter[3] += _registers[4]; break;
+                    }
+
+                    if ((_registers[6] & 0x04) == 0x00)
+                    {
+                        // periodic noise
+                        _lsfr = (ushort)((_lsfr >> 1) | ((_lsfr & 1) << 15));
+                        _channelOutput[3] = _lsfr & 1;
+                    }
+                    else
+                    {
+                        // white noise
+                        _lsfr = (ushort)((_lsfr >> 1) | (parity(_lsfr & 0x09) << 15));
+                        _channelOutput[3] = _lsfr & 1;
+                    }
+                }
+
+                // mix channel output and output sample
+                int sample = 0;
+
+                for (int i = 0; i < ChannelCount; i++)
+                {
+                    sample += (int)(_volumeTable[_registers[(i * 2) + 1]] * _channelOutput[i]);
+                }
+
+                _outputStream.Write((short)sample);
+
+                _clock += 16;
+            }
+        }
+
+        private int parity(int v)
+        {
+            v ^= v >> 8;
+            v ^= v >> 4;
+            v ^= v >> 2;
+            v ^= v >> 1;
+
+            return v;
+        }
+    }
+}
